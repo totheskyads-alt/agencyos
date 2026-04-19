8fe29b22ade4dc73d42862954b16b33ae74993c3
totheskyads-alt <totheskyads@gmail.com>
2026-04-19 21:49:08 +0200

    Add files via upload

--- agencyos/app/layout.js
+++ agencyos/app/layout.js
@@ -1,17 +1,20 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'Agency OS',
-  description: 'Operating system pentru agenția ta',
+  title: 'Sky Metrics',
+  description: 'Project & time management for agencies',
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-icon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ro">
+    <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
       </head>
       <body>{children}</body>
     </html>
