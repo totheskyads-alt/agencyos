decab3044a2ee999fbd0cba09b593bf080485e2a
Mihai Jdovanu <mihaijdovanu@Mihais-MacBook-Air.local>
2026-04-27 00:29:40 +0200

    GTM in layout.js (fix iframe detection)

--- agencyos/app/layout.js
+++ agencyos/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   metadataBase: new URL('https://www.sky-metrics.online'),
@@ -67,8 +68,23 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" type="image/png" href="/icon-192.png" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T9XM93W3');
+        `}</Script>
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9XM93W3"
+            height="0" width="0" style={{display:'none',visibility:'hidden'}} />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
